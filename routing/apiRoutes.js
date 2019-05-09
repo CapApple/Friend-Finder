4e3203a8afe0a62f426daf8a7e8cad12149149ab
Jeremy <jeremy.cai@ymail.com>
2019-05-08 22:02:38 -0500

    fixing the issue of integer being posted as string; fixing appending pic

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -7,7 +7,13 @@ module.exports = function(app){
     });
     // post request, handle user submits and push them into array
     app.post("/api/users", function(req, res){
+        // This is interesting! I should write this in my blog!
+        req.body.scores = req.body.scores.map(function(item){
+            return parseInt(item);
+        });
+        
         usersData.push(req.body);
+        console.log(req.body);
         res.json(usersData);
     })
    
